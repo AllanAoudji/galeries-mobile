6aa4eaac58d416fc599422d5bfdedb92534dd313
AZIHubert <45357293+AZIHubert@users.noreply.github.com>
2021-08-16 19:47:45 +0200

    add a helper Loader component

--- App.tsx
+++ App.tsx
@@ -1,36 +1,27 @@
 import 'react-native-gesture-handler';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { Provider } from 'react-redux';
 
 import { Notification } from '#components';
 import ThemeProvider from '#contexts/ThemeContext';
+import Loader from '#helpers/Loader';
 import RootStack from '#screens/RootStack';
 import store from '#store';
 
 export default function App() {
-    const [loaded] = useFonts({
-        HelveticaLtStBold: require('./assets/fonts/HelveticaLTStd-Bold.otf'),
-        HelveticaLtStLight: require('./assets/fonts/HelveticaLTStd-Light.otf'),
-        HelveticaLtStObl: require('./assets/fonts/HelveticaLTStd-Obl.otf'),
-        HelveticaLtStRoman: require('./assets/fonts/HelveticaLTStd-Roman.otf'),
-    });
-
-    if (!loaded) {
-        return null;
-    }
-
     return (
         <ThemeProvider>
             <Provider store={store}>
-                <NavigationContainer>
-                    <RootStack />
-                    <Notification />
-                    <StatusBar style="auto" />
-                </NavigationContainer>
+                <Loader>
+                    <NavigationContainer>
+                        <RootStack />
+                        <Notification />
+                        <StatusBar style="auto" />
+                    </NavigationContainer>
+                </Loader>
             </Provider>
         </ThemeProvider>
     );
