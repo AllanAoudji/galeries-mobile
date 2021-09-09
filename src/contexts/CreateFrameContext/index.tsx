e804d4be7dd788ec8e5206892c522a6f5600d5a0
AZIHubert <45357293+AZIHubert@users.noreply.github.com>
2021-09-09 15:42:12 +0200

    check that post frame works

--- src/contexts/CreateFrameContext/index.tsx
+++ src/contexts/CreateFrameContext/index.tsx
@@ -4,23 +4,32 @@ import Animated, {
     useAnimatedReaction,
     runOnJS,
 } from 'react-native-reanimated';
+import { useSelector } from 'react-redux';
+
+import { AxiosError } from 'axios';
+import { END_POINT } from '#helpers/constants';
+import request from '#helpers/request';
+import { currentGalerieSelector } from '#store/selectors';
 
 export const CreateFrameContext = React.createContext<{
     addPictures: (uri: string, callBack?: () => void) => void;
     picturesUri: string[];
     positions: Animated.SharedValue<DragAndDrop.Positions>;
+    postFrame: (description: string) => void;
     removePictures: (uri: string, callback?: () => void) => void;
     switchPosition: (id: string, oldOrder: number, newOrder: number) => void;
 }>({
     addPictures: () => {},
     picturesUri: [],
     positions: { value: {} },
+    postFrame: () => {},
     removePictures: () => {},
     switchPosition: () => {},
 });
 
 export const CreateFrameProvider: React.FC<{}> = ({ children }) => {
     const [picturesUri, setPicturesUri] = React.useState<string[]>([]);
+    const currentGalerie = useSelector(currentGalerieSelector);
 
     const positions = useSharedValue<DragAndDrop.Positions>({});
 
@@ -35,6 +44,39 @@ export const CreateFrameProvider: React.FC<{}> = ({ children }) => {
             if (callBack) callBack();
         },
         []
+    );
+    const postFrame = React.useCallback(
+        async (description: string) => {
+            if (currentGalerie && picturesUri.length) {
+                const formData = new FormData();
+                picturesUri.forEach((pictureUri) => {
+                    formData.append('image', {
+                        // @ts-ignore
+                        uri: pictureUri,
+                        type: 'image/jpg',
+                        name: pictureUri,
+                    });
+                });
+                formData.append('description', description);
+                request({
+                    body: formData,
+                    method: 'POST',
+                    url: END_POINT.GALERIE_FRAMES(currentGalerie.id),
+                    contentType: 'multipart/form-data',
+                })
+                    .then((response) => {
+                        console.log(response.data.data);
+                    })
+                    .catch((err: AxiosError) => {
+                        console.log('error');
+                        if (err.response) {
+                            console.log(err.response.data);
+                            console.log(err.response.status);
+                        }
+                    });
+            }
+        },
+        [currentGalerie, picturesUri]
     );
     const removePictures = React.useCallback(
         (uri: string, callback?: () => void) => {
@@ -90,6 +132,7 @@ export const CreateFrameProvider: React.FC<{}> = ({ children }) => {
                 addPictures,
                 picturesUri,
                 positions,
+                postFrame,
                 removePictures,
                 switchPosition,
             }}
--- src/screens/CreateFrameStackNavigator/AddDescriptionScreen/index.tsx
+++ src/screens/CreateFrameStackNavigator/AddDescriptionScreen/index.tsx
@@ -1,7 +1,44 @@
 import * as React from 'react';
+import { View } from 'react-native';
 
-import { Typography } from '#components';
+import { CustomButton, FormScreen } from '#components';
+import { CreateFrameContext } from '#contexts/CreateFrameContext';
 
-const AddDescriptionScreen = () => <Typography>Add description</Typography>;
+type Props = {
+    navigation: Screen.CreateFrameStack.AddDescriptionNavigationProp;
+};
+
+const AddDescriptionScreen = ({ navigation }: Props) => {
+    const { postFrame } = React.useContext(CreateFrameContext);
+
+    const handlePostFrame = React.useCallback(() => {
+        postFrame('');
+    }, [postFrame]);
+    const handleReturn = React.useCallback(() => {
+        navigation.navigate('AddPictures');
+    }, [navigation]);
+
+    return (
+        <FormScreen
+            handleOnPressReturn={handleReturn}
+            renderBottom={
+                <>
+                    <CustomButton
+                        mb="smallest"
+                        onPress={handlePostFrame}
+                        title="post frame"
+                    />
+                    <CustomButton
+                        onPress={handleReturn}
+                        title="return"
+                        variant="stroke"
+                    />
+                </>
+            }
+            renderTop={<View></View>}
+            title="add a description (optinal)"
+        />
+    );
+};
 
 export default AddDescriptionScreen;
