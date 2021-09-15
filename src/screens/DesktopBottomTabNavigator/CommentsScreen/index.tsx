596330c75185fae66c9fa49e5bb4f918462ed6ac
AZIHubert <45357293+AZIHubert@users.noreply.github.com>
2021-09-15 12:24:24 +0200

    add header to CommentsScreen

--- src/screens/DesktopBottomTabNavigator/CommentsScreen/index.tsx
+++ src/screens/DesktopBottomTabNavigator/CommentsScreen/index.tsx
@@ -1,7 +1,39 @@
 import * as React from 'react';
+import Animated from 'react-native-reanimated';
+import styled from 'styled-components/native';
 
-import { Typography } from '#components';
+import { GLOBAL_STYLE } from '#helpers/constants';
+import { useComponentSize, useHideHeaderOnScroll } from '#hooks';
+import { DefaultHeader } from '#components';
 
-const CommentScreen = () => <Typography>Comment</Typography>;
+const Container = styled.View`
+    background-color: ${({ theme }) => theme.colors['secondary-light']};
+    flex: 1;
+`;
+const Header = styled(Animated.View)`
+    background-color: ${({ theme }) => theme.colors['secondary-light']};
+    position: absolute;
+    width: 100%;
+    z-index: 10;
+`;
+
+const CommentScreen = () => {
+    const { onLayout } = useComponentSize();
+    const { containerStyle, headerStyle } = useHideHeaderOnScroll(
+        GLOBAL_STYLE.HEADER_TAB_HEIGHT
+    );
+
+    return (
+        <Container>
+            <Header onLayout={onLayout} style={containerStyle}>
+                <DefaultHeader
+                    style={headerStyle}
+                    title="comments"
+                    variant="secondary"
+                />
+            </Header>
+        </Container>
+    );
+};
 
 export default CommentScreen;
