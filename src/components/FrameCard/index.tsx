76e36c58349fd15375ba7b534af620b432f590eb
AZIHubert <45357293+AZIHubert@users.noreply.github.com>
2021-09-16 18:37:19 +0200

    create useLike

--- src/components/FrameCard/index.tsx
+++ src/components/FrameCard/index.tsx
@@ -1,10 +1,6 @@
-import { AxiosError } from 'axios';
 import * as React from 'react';
-import { useDispatch } from 'react-redux';
 
-import { END_POINT, ERROR_MESSAGE } from '#helpers/constants';
-import request from '#helpers/request';
-import { setFrames, setNotification } from '#store/actions';
+import { useLike } from '#hooks';
 
 import Footer from './Footer';
 import Header from './Header';
@@ -24,87 +20,14 @@ type Props = {
 };
 
 const FrameCard = ({ frame, onPressComments, onPressLikes }: Props) => {
-    const dispatch = useDispatch();
+    const { like } = useLike();
 
     const handlePressComments = React.useCallback(
         () => onPressComments(frame.id),
         [frame]
     );
     const handlePressLike = React.useCallback(() => {
-        request({
-            body: {},
-            method: 'POST',
-            url: END_POINT.LIKES(frame.id),
-        })
-            .then((res) => {
-                if (
-                    res.data.data &&
-                    res.data.data.liked !== undefined &&
-                    res.data.data.numOfLikes !== undefined
-                ) {
-                    dispatch(
-                        setFrames({
-                            data: {
-                                byId: {
-                                    [frame.id]: {
-                                        autoIncrementId: frame.autoIncrementId,
-                                        createdAt: frame.createdAt,
-                                        comments: frame.comments,
-                                        description: frame.description,
-                                        galerieId: frame.galerieId,
-                                        galeriePicturesId:
-                                            frame.galeriePicturesId,
-                                        id: frame.id,
-                                        liked: res.data.data.liked,
-                                        likes: frame.likes,
-                                        numOfComments: frame.numOfComments,
-                                        numOfLikes: res.data.data.numOfLikes,
-                                        updatedAt: frame.updatedAt,
-                                        userId: frame.userId,
-                                    },
-                                },
-                            },
-                        })
-                    );
-                } else {
-                    dispatch(
-                        setNotification({
-                            status: 'error',
-                            text: ERROR_MESSAGE.DEFAULT_ERROR_MESSAGE,
-                        })
-                    );
-                }
-            })
-            .catch((err: AxiosError) => {
-                if (err.response) {
-                    if (
-                        err.response.data &&
-                        err.response.data.errors &&
-                        typeof err.response.data.errors === 'string'
-                    ) {
-                        dispatch(
-                            setNotification({
-                                text: err.response.data.errors,
-                                status: 'error',
-                            })
-                        );
-                    } else {
-                        dispatch(
-                            setNotification({
-                                status: 'error',
-                                text: ERROR_MESSAGE.DEFAULT_ERROR_MESSAGE,
-                            })
-                        );
-                    }
-                } else {
-                    dispatch(
-                        setNotification({
-                            status: 'error',
-                            text: ERROR_MESSAGE.DEFAULT_ERROR_MESSAGE,
-                        })
-                    );
-                }
-            });
+        like(frame);
     }, [frame]);
     const handlePressLikes = React.useCallback(() => {
         if (+frame.numOfLikes > 0) onPressLikes(frame.id);
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,6 +2,7 @@ export { default as useCameraRoll } from './useCameraRoll';
 export { default as useComponentSize } from './useComponentSize';
 export { default as useHideHeaderOnScroll } from './useHideHeaderOnScroll';
 export { default as useKeyboard } from './useKeyboard';
+export { default as useLike } from './useLike';
 export { default as useLogin } from './useLogin';
 export { default as usePostFrame } from './usePostFrame';
 export { default as usePostGalerie } from './usePostGalerie';
