e847059df96e77a4fb37be8c0a9aa93efe6ba290
AZIHubert <45357293+AZIHubert@users.noreply.github.com>
2021-10-18 10:32:24 +0200

    remove console.log

--- src/store/galeriePictures/middlewares/putGaleriePicturesMiddleware/index.ts
+++ src/store/galeriePictures/middlewares/putGaleriePicturesMiddleware/index.ts
@@ -12,7 +12,6 @@ const putGaleriePicturesMiddleware: Middleware<{}, Store.Reducer> =
 
         if (action.type !== GALERIE_PICTURES_PUT) return;
 
-        console.log(action);
         const frameId = action.meta.query
             ? action.meta.query.frameId
             : undefined;
